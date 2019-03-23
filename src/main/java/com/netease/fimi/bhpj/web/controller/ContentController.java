8b97c4902b72ff07a114b6dc5775c06dfc000f8f
fimi.zhuang <725459159@qq.com>
2019-03-23 09:51:13 +0800

    commit 2019.03.23 9:51

--- src/main/java/com/netease/fimi/bhpj/web/controller/ContentController.java
+++ src/main/java/com/netease/fimi/bhpj/web/controller/ContentController.java
@@ -4,13 +4,20 @@ import com.google.common.collect.Maps;
 import com.netease.fimi.bhpj.domain.Content;
 import com.netease.fimi.bhpj.service.ContentService;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +26,9 @@ import java.util.Map;
 public class ContentController {
     @Autowired
     private ContentService contentService;
+
+    @Value("${image.upload.basePath}")
+    private String basePath;
 
     private static Logger log = LoggerFactory.getLogger(ContentController.class);
 
@@ -61,6 +71,42 @@ public class ContentController {
         json.put("msg", String.format("delete a content where id is %d", id));
         json.put("code", 1);
         contentService.deleteContentById(id);
+        return json;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/upload_content_images",method = RequestMethod.POST)
+    @ApiOperation(value = "上传content 本地图片", notes = "传入content的id 和 file")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "file", value = "需要上传的图片 file", required = true, dataType = "MultipartFile"),
+            @ApiImplicitParam(name = "id", value = "内容content的主键id", required = true)
+    })
+
+    public Map<String, Object> uploadContentImages(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        Integer id = Integer.valueOf(request.getParameter("id"));
+        Content content = contentService.getContentById(id);
+        log.info("access api /upload_foodstuff_images where id is {}", id);
+
+        String folderPath1 = this.basePath.split("&")[0] + id;
+        File folder1 = new File(folderPath1);
+        if (!folder1.exists()) {
+            folder1.mkdir();
+        }
+
+        String path1 = folderPath1 + "/" + file.getOriginalFilename();
+        FileOutputStream out = new FileOutputStream(path1);
+        out.write(file.getBytes());
+        out.flush();
+        out.close();
+
+        //update content
+        content.setImagePath(file.getOriginalFilename());
+        content.setImageType(1);
+        contentService.modifyContentById(content);
+        log.info("uploaded a image where path is {}", path1);
+
+        Map<String, Object> json = Maps.newHashMap();
+        json.put("msg", "successfully! upload content image where id = " + id);
         return json;
     }
 }
